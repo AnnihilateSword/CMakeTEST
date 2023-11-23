5e18150952644f779f6d7907685b199602c3cc2c
Jinkun <743144825@qq.com>
2023-11-23 17:29:17 +0800

    Added glfw submodule and Update CMake.

--- CMakeTEST/src/Main.cpp
+++ CMakeTEST/src/Main.cpp
@@ -3,7 +3,10 @@
 #include <cstdlib>
 #include <iostream>
 
-int main() {
+#include <GLFW/glfw3.h>
+
+int main()
+{
   Message say_hello("Hello, CMake World!");
 
   std::cout << say_hello << std::endl;
@@ -12,6 +15,38 @@ int main() {
 
   std::cout << say_goodbye << std::endl;
 
-  std::cin.get();
+  // GLFW Example
+  GLFWwindow* window;
+
+  /* Initialize the library */
+  if (!glfwInit())
+	  return -1;
+
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window)
+  {
+	  glfwTerminate();
+	  return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window))
+  {
+	  /* Render here */
+	  glClear(GL_COLOR_BUFFER_BIT);
+
+	  /* Swap front and back buffers */
+	  glfwSwapBuffers(window);
+
+	  /* Poll for and process events */
+	  glfwPollEvents();
+  }
+
+  glfwTerminate();
+
   return EXIT_SUCCESS;
 }
